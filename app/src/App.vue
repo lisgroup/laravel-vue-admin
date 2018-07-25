e3d601f8e65eda234f8a05c2b70a3beb1171bb37
lisgroup <407505297@qq.com>
2018-07-25 18:14:13 +0800

    add index

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar></nav-bar>
+    <!--<img src="./assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
+//VueRouter:引入路由对象
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 引入 layUI css
+import 'layui-src/dist/css/layui.css'
+
+// 引入全局组件需要的组件对象 开始
+import NavBar from './components/common/navBar.vue';
+// 引入全局组件需要的组件对象 开始
+Vue.component('NavBar', NavBar); // 使用最好以 nav-bar
+
+// 引入自己的vue文件 开始
 import App from './App.vue'
+import Home from './components/home/home.vue'
 
 Vue.config.productionTip = false
 
+// VueRouter:创建对象并配置路由规则！！！导航
+let router = new VueRouter({
+  // VueRouter：配置路由规则
+  routes: [
+    { path: '/', redirect: { name: 'home' } },
+    { path: '/home', name: 'home', component: Home }
+  ]
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
