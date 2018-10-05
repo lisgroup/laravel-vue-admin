aff01e3a9a25699d5bc35dcea0ec53db16adf138
lisgroup <407505297@qq.com>
2018-10-05 09:42:19 +0800

    update 配置

--- php/public/index.php
+++ php/public/index.php
@@ -13,5 +13,13 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
+
+//解决虚拟主机或二级目录下的css样式找不到的问题
+if (!defined('__ROOT__'))
+{
+    $_root = rtrim(dirname(trim($_SERVER['SCRIPT_NAME'], '/')), '/');
+    define('__ROOT__', (($_root == '/' || $_root == '\\') ? '' : $_root));
+}
+
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
