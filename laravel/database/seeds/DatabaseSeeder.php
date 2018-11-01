df697e2a779b05ffe15e8fcb8ad4bf5185b64f51
lisgroup <407505297@qq.com>
2018-11-01 17:55:33 +0800

    update: 默认的 DatabaseSeeder 类中的 run 方法才会执行迁移

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(BusLinesTablesSeeder::class);
+        $this->call(CronTasksTableSeeder::class);
     }
 }
