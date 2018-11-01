db7e9d13a75f9c7b03f200c06e9a3aedfba21627
lisgroup <407505297@qq.com>
2018-11-01 16:21:29 +0800

    update: 入库允许修改的字段列表

--- laravel/app/Models/BusLine.php
+++ laravel/app/Models/BusLine.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class BusLine extends Model
 {
-    //
+    /**
+     * 与模型关联的数据表
+     *
+     * @var string
+     */
+    // protected $table = 'bus_lines';
+    protected $fillable = ['name', 'cid', 'LineGuid', 'LineInfo', 'FromTo', 'expiration'];
 }
