81e7a4a548b51f72a624cb2c19fb2ad30677bb5b
lisgroup <407505297@qq.com>
2018-11-03 22:06:43 +0800

    add: 任务仓库类

---
+++ laravel/app/Http/Repository/TaskRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Desc: TaskRepository 仓库类
+ * User: lisgroup
+ * Date: 18-11-03
+ * Time: 22:06
+ */
+
+namespace App\Http\Repository;
+
+
+use QL\QueryList;
+
+class TaskRepository
+{
+
+}
