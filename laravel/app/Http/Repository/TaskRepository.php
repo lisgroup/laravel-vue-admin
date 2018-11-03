4d45ad6f1e1e65be592ab999a8a564bd40c220c7
lisgroup <407505297@qq.com>
2018-11-03 22:10:51 +0800

    update: 任务仓库类

--- laravel/app/Http/Repository/TaskRepository.php
+++ laravel/app/Http/Repository/TaskRepository.php
@@ -13,5 +13,39 @@ use QL\QueryList;
 
 class TaskRepository
 {
+    /**
+     * @var mixed|QueryList
+     */
+    protected $ql;
+    /**
+     * @var string $url 定义采集的 url
+     */
+    protected $url = '';
 
+    private static $instance;
+
+    // private $cronModel;
+
+    public static function getInstent($conf = [])
+    {
+        if (!isset(self::$instance) || !(self::$instance instanceof TaskRepository)) {
+            self::$instance = new static($conf);
+        }
+        return self::$instance;
+    }
+
+    private function __construct($config)
+    {
+        if (!empty($config)) {
+            foreach ($config as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+        $this->ql || $this->ql = QueryList::getInstance();
+    }
+
+    private function __clone()
+    {
+
+    }
 }
