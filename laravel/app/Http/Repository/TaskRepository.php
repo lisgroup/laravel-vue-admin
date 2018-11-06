a6fd2b82ee790f32bad13d36d55a0d3e130db49c
lisgroup <407505297@qq.com>
2018-11-06 22:56:27 +0800

    fix: 本地执行全部任务

--- laravel/app/Http/Repository/TaskRepository.php
+++ laravel/app/Http/Repository/TaskRepository.php
@@ -47,78 +47,86 @@ class TaskRepository
             return ['code' => 1, 'msg' => 'error'];
         }
 
-        // $url = 'http://bus.suzhou.bendibao.com/linelist/1.htm';
-        // $ql = $this->ql->get($url);
-        // 手动转码
-        // $html = iconv('GBK', 'UTF-8', $html);
-        // print_r($html);
-        $url = 'http://bus.suzhou.bendibao.com/linelist/2.htm';
-        $ql = $this->ql->get($url);
-        // 1. 元数据采集规则
-        $rules = [
-            'line' => ['#listall li>a', 'text'],
-            'href' => ['#listall li>a', 'href']
+        // 正常需抓取页面列表，下面示例只是简单操作
+        $urls = [
+            'http://bus.suzhou.bendibao.com/linelist/1.htm',
+            'http://bus.suzhou.bendibao.com/linelist/2.htm',
         ];
-        $result = $ql->rules($rules)->encoding('UTF-8', 'GB2312')->removeHead()->queryData();
-        // var_dump($result[0]);exit();
-
-        // 2. 采集详细车次线路信息
-        $url = 'http://bus.suzhou.bendibao.com';
-        foreach ($result as $value) {
-            $listInfo = $this->ql->get($url.$value['href']);
-            $try = 1;
-            // 如果获取数据失败，再尝试2次
-            while (empty($listInfo)) {
-                if ($try >= 3) {
-                    break;
-                }
-                $listInfo = $this->ql->get($url.$value['href']);
-
-                $try++;
-            }
-
+        foreach ($urls as $url) {
+            // $url = 'http://bus.suzhou.bendibao.com/linelist/1.htm';
+            // $ql = $this->ql->get($url);
+            // 手动转码
+            // $html = iconv('GBK', 'UTF-8', $html);
+            // print_r($html);
+            // $url = 'http://bus.suzhou.bendibao.com/linelist/2.htm';
+            $ql = $this->ql->get($url);
+            // 1. 元数据采集规则
             $rules = [
-                'name' => ['#rpt_Line_List_ctl00_lk_Line', 'text'],
-                'open_time' => ['#rpt_Line_List_ctl00_lb_YYSJ>span', 'text'], // 营运时间
-                'depart_time' => ['#rpt_Line_List_ctl00_lb_FCJG>span', 'text'], // 发车间隔
-                'price' => ['#rpt_Line_List_ctl00_lb_Price>span', 'text'],
-                'company' => ['#rpt_Line_List_ctl00_lb_ComName>span', 'text'], // 公交公司
-                'station' => ['#rpt_Line_List_ctl00_lb_StationAll1', 'text'], // 去程
-                'station_back' => ['#rpt_Line_List_ctl00_lb_StationAll2', 'text'], // 返程
-                'last_update' => ['#rpt_Line_List_ctl00_lb_UpdateTime', 'text'], // 最后更新日期
+                'line' => ['#listall li>a', 'text'],
+                'href' => ['#listall li>a', 'href']
             ];
-            $rs = $listInfo->rules($rules)->encoding('UTF-8', 'GB2312')->removeHead()->queryData();
-            if (!empty($rs)) {
-                $line = $rs[0];
-                $line['station'] = str_replace([" ", "　", "\n", "\r", "\t"], ['', '', '', '', ''], $line['station']);
-                $line['station_back'] = str_replace([" ", "　", "\n", "\r", "\t"], ['', '', '', '', ''], $line['station_back']);
-                // 抓取的数据入库操作
-                // $model = new Line($line);
-                // $model->save();
-                $day = date('Y-m-d H:i:s');
-                // 2. 插入前判断是否已经存在该线路
-                $getLine = Line::where('name', $line['name'])->get();
-                if ($getLine) {
-                    // 2.1 update 操作
-                    $line += ['updated_at' => $day];
-                    if (Line::where('id', $getLine['id'])->update($line)) {
-                        Log::info('Lines 更新执行成功 success: 线路名称 '.$line['name']);
-                    } else {
-                        Log::error('Lines 更新执行失败 error: 线路名称 '.$line['name'], $line);
+            $result = $ql->rules($rules)->encoding('UTF-8', 'GB2312')->removeHead()->queryData();
+            // var_dump($result[0]);exit();
+
+            // 2. 采集详细车次线路信息
+            $url = 'http://bus.suzhou.bendibao.com';
+            foreach ($result as $value) {
+                $listInfo = $this->ql->get($url.$value['href']);
+                $try = 1;
+                // 如果获取数据失败，再尝试2次
+                while (empty($listInfo)) {
+                    if ($try >= 3) {
+                        break;
                     }
+                    $listInfo = $this->ql->get($url.$value['href']);
+
+                    $try++;
                 }
 
-                // 2.2 插入操作
-                $line += ['created_at' => $day, 'updated_at' => $day];
-                if (Line::insert($line)) {
-                    Log::info('Lines 入库执行成功 success: 线路名称 '.$line['name']);
+                $rules = [
+                    'name' => ['#rpt_Line_List_ctl00_lk_Line', 'text'],
+                    'open_time' => ['#rpt_Line_List_ctl00_lb_YYSJ>span', 'text'], // 营运时间
+                    'depart_time' => ['#rpt_Line_List_ctl00_lb_FCJG>span', 'text'], // 发车间隔
+                    'price' => ['#rpt_Line_List_ctl00_lb_Price>span', 'text'],
+                    'company' => ['#rpt_Line_List_ctl00_lb_ComName>span', 'text'], // 公交公司
+                    'station' => ['#rpt_Line_List_ctl00_lb_StationAll1', 'text'], // 去程
+                    'station_back' => ['#rpt_Line_List_ctl00_lb_StationAll2', 'text'], // 返程
+                    'last_update' => ['#rpt_Line_List_ctl00_lb_UpdateTime', 'text'], // 最后更新日期
+                ];
+                $rs = $listInfo->rules($rules)->encoding('UTF-8', 'GB2312')->removeHead()->queryData();
+                if (!empty($rs)) {
+                    $line = $rs[0];
+                    $line['station'] = str_replace([" ", "　", "\n", "\r", "\t"], ['', '', '', '', ''], $line['station']);
+                    $line['station_back'] = str_replace([" ", "　", "\n", "\r", "\t"], ['', '', '', '', ''], $line['station_back']);
+                    // 抓取的数据入库操作
+                    // $model = new Line($line);
+                    // $model->save();
+                    $day = date('Y-m-d H:i:s');
+                    // 2. 插入前判断是否已经存在该线路
+                    $getLine = Line::where('name', $line['name'])->get();
+                    if ($getLine) {
+                        // 2.1 update 操作
+                        $line += ['updated_at' => $day];
+                        if (Line::where('id', $getLine['id'])->update($line)) {
+                            Log::info('Lines 更新执行成功 success: 线路名称 '.$line['name']);
+                        } else {
+                            Log::error('Lines 更新执行失败 error: 线路名称 '.$line['name'], $line);
+                        }
+                    }
+
+                    // 2.2 插入操作
+                    $line += ['created_at' => $day, 'updated_at' => $day];
+                    if (Line::insert($line)) {
+                        Log::info('Lines 入库执行成功 success: 线路名称 '.$line['name']);
+                    } else {
+                        Log::error('Lines 入库执行失败 error: 线路名称 '.$line['name'], $line);
+                    }
                 } else {
-                    Log::error('Lines 入库执行失败 error: 线路名称 '.$line['name'], $line);
+                    Log::error('Lines 线路获取失败 error: 线路 URL '.$url.$value['href']);
                 }
+                // sleep 200ms
+                usleep(200000);
             }
-            // sleep 200ms
-            usleep(200000);
-
         }
         return ['code' => 0, 'msg' => 'success'];
     }
