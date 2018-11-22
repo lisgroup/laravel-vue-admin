c5cb06c7b7c6b79699e2b9d9b1d2988088f3f364
lisgroup <407505297@qq.com>
2018-11-22 17:01:51 +0800

    fix: add 后台管理员账号迁移

---
+++ laravel/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's clear the users table first
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Let's make sure everyone has the same password and
+        // let's hash it before the loop, or else our seeder
+        // will be too slow.
+        $password = Hash::make('123456');
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+    }
+}
