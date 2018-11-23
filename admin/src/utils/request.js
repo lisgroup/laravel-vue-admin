0b3e27b4f0533dac6afa9dbb7bd0708c749b31d0
lisgroup <407505297@qq.com>
2018-11-23 18:15:11 +0800

    fix: code

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -28,10 +28,10 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   response => {
     /**
-     * code为非20000是抛错 可结合自己业务进行修改
+     * code 为非 200 是抛错 可结合自己业务进行修改
      */
     const res = response.data
-    if (res.code !== 20000) {
+    if (res.code !== 200) {
       Message({
         message: res.message,
         type: 'error',
