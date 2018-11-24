0651991204be2fdeadfe3bfdd04b74314abcd938
lisgroup <407505297@qq.com>
2018-11-24 22:59:07 +0800

    add: restfull api 控制器

---
+++ laravel/app/Http/Controllers/Api/CronTaskController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CronTaskController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     * 要求附带email和password（数据来源users表）
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // 这里额外注意了：官方文档样例中只除外了『login』
+        // 这样的结果是，token 只能在有效期以内进行刷新，过期无法刷新
+        // 如果把 refresh 也放进去，token 即使过期但仍在刷新期以内也可刷新
+        // 不过刷新一次作废
+        // $this->middleware('auth:api', ['except' => ['login', 'adminLogin']]);
+        // 另外关于上面的中间件，官方文档写的是『auth:api』
+        // 但是我推荐用 『jwt.auth』，效果是一样的，但是有更加丰富的报错信息返回
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
