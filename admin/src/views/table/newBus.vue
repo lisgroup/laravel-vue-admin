a129ab06c684aa027f1609a6bd38e45263a5712a
lisgroup <407505297@qq.com>
2018-11-27 17:19:21 +0800

    fix: 表单字段命名

--- admin/src/views/table/newBus.vue
+++ admin/src/views/table/newBus.vue
@@ -1,35 +1,30 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name"/>
+      <el-form-item label="车次名称">
+        <el-input v-model="form.LineInfo"/>
       </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai"/>
-          <el-option label="Zone two" value="beijing"/>
-        </el-select>
+      <el-form-item label="cid">
+        <el-input v-model="form.cid"/>
       </el-form-item>
-      <el-form-item label="Activity time">
+
+      <el-form-item label="LineGuid">
+        <el-input v-model="form.LineGuid"/>
+      </el-form-item>
+
+      <el-form-item label="起始时间">
         <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;"/>
+          <el-date-picker v-model="form.start_at" type="date" placeholder="Pick a date" style="width: 100%;"/>
         </el-col>
         <el-col :span="2" class="line">-</el-col>
         <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;"/>
+          <el-time-picker v-model="form.end_at" type="fixed-time" placeholder="Pick a time" style="width: 100%;"/>
         </el-col>
       </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery"/>
+      <el-form-item label="是否启动">
+        <el-switch v-model="form.is_task"/>
       </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type"/>
-          <el-checkbox label="Promotion activities" name="type"/>
-          <el-checkbox label="Offline activities" name="type"/>
-          <el-checkbox label="Simple brand exposure" name="type"/>
-        </el-checkbox-group>
-      </el-form-item>
+
       <el-form-item label="Resources">
         <el-radio-group v-model="form.resource">
           <el-radio label="Sponsor"/>
@@ -52,12 +47,12 @@ export default {
   data() {
     return {
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
+        LineInfo: '',
+        cid: '',
+        LineGuid: '',
+        start_at: '',
+        end_at: '',
+        is_task: false,
         resource: '',
         desc: ''
       }
