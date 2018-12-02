a94ba6de80ea72e9b417f51c713ba47663d427d6
lisgroup <407505297@qq.com>
2018-12-02 14:46:45 +0800

    fix: 重置问题

--- admin/src/views/table/newBus.vue
+++ admin/src/views/table/newBus.vue
@@ -26,7 +26,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit('form')">提交</el-button>
-        <el-button @click="resetForm">重置</el-button>
+        <el-button @click="resetForm('form')">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
