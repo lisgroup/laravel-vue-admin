136682f3f48c117092b2d9b66b6ec71c7487f2bb
lisgroup <407505297@qq.com>
2018-12-16 14:22:24 +0800

    fix(view): add package layui-src

--- admin/src/main.js
+++ admin/src/main.js
@@ -6,6 +6,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
+// 引入 layUI css
+import 'layui-src/dist/css/layui.css'
+
 import '@/styles/index.scss' // global css
 
 import App from './App'
