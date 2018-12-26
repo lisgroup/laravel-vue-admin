19cb422e000e0fddac5c0e7bf495d49c34667809
lisgroup <407505297@qq.com>
2018-12-26 18:24:10 +0800

    fix(view): Page jump problem

--- admin/src/permission.js
+++ admin/src/permission.js
@@ -10,7 +10,7 @@ router.beforeEach((to, from, next) => {
   NProgress.start()
   if (getToken()) {
     if (to.path === '/login') {
-      next({ path: '/' })
+      next({ path: '/admin' })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
       if (store.getters.roles.length === 0) {
@@ -19,7 +19,7 @@ router.beforeEach((to, from, next) => {
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
             Message.error(err || 'Verification failed, please login again')
-            next({ path: '/' })
+            next({ path: '/admin' })
           })
         })
       } else {
