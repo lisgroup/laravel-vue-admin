6cc5958b235cbba288beece0255046cabebe5ae4
lisgroup <407505297@qq.com>
2019-01-09 14:39:39 +0800

    fix(control): success

--- laravel/app/Http/Controllers/Controller.php
+++ laravel/app/Http/Controllers/Controller.php
@@ -21,7 +21,7 @@ class Controller extends BaseController
     public function out($code = 200, $data = [], $reason = 'success')
     {
         if ($reason === 'success') {
-            $reason = config('errorCode.'.$code.'.reason') ?? 'error';
+            $reason = config('errorCode.'.$code.'.reason') ?? 'success';
         }
 
         return response()->json(compact('code', 'reason', 'data'))->setEncodingOptions(JSON_UNESCAPED_UNICODE);
