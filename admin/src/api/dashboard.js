0d160a0c3620709e9a3737e82e0daf9b44a23d78
lisgroup <407505297@qq.com>
2019-01-19 20:45:26 +0800

    fix(view): url

--- admin/src/api/dashboard.js
+++ admin/src/api/dashboard.js
@@ -3,40 +3,8 @@ import request from '@/utils/request'
 export function getList(params) {
   // return request.get('/api/lines', params)
   return request({
-    url: '/api/admin/index',
+    url: '/api/admin/system',
     method: 'get',
     params
   })
-}
-
-export function postAdd(params) {
-  return request.post('/api/lines', params)
-}
-
-export function edit(id) {
-  return request.get('/api/lines/' + id)
-}
-
-export function postEdit(id, params) {
-  return request.patch('/api/lines/' + id, params)
-}
-
-export function deleteAct(id) {
-  return request.delete('/api/lines/' + id)
-}
-
-/**
- * 搜索 lines
- * @param params
- */
-export function getLines(params) {
-  return request({
-    url: '/api/line_search',
-    method: 'get',
-    params
-  })
-}
-
-export function clearCache(params) {
-  return request.post('/api/clearCache/', params)
 }
