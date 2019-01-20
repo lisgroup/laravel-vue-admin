e83bc0e1ffb72d81fa575406b44bc943b1c5216d
lisgroup <407505297@qq.com>
2019-01-20 23:07:37 +0800

    update: table

--- laravel/database/migrations/2019_01_20_230555_create_links_table.php
+++ laravel/database/migrations/2019_01_20_230555_create_links_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateFriendshipLinksTable extends Migration {
+class CreateLinksTable extends Migration {
 
 	/**
 	 * Run the migrations.
