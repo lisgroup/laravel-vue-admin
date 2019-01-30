87f051295ec8b50c83f9857f415c2521f75938ec
lisgroup <407505297@qq.com>
2019-01-30 23:26:24 +0800

    test: 测试数据

--- laravel/app/Http/Controllers/Api/ApiExcelController.php
+++ laravel/app/Http/Controllers/Api/ApiExcelController.php
@@ -10,6 +10,8 @@ use App\Models\ApiExcel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class ApiExcelController extends Controller
 {
@@ -101,11 +103,56 @@ class ApiExcelController extends Controller
         // $result = $multi->multiRequest('http://118.25.87.12/token/php/index.php/hello/123', '111');
 
         $result  = [
-            ['name' => 'lis', 'age' => 12, 'size' => '60'],
-            ['name' => 'zha', 'age' => 20, 'size' => '160'],
-            ['name' => 'hut', 'age' => 36, 'size' => '260'],
+            ['name' => 'lis', 'idcard' => 12, 'bankcard' => '111', 'error_code' => 10, 'reason' => 'error'],
+            ['name' => 'zha', 'idcard' => 20, 'bankcard' => '160', 'error_code' => 10, 'reason' => 'error'],
+            ['name' => 'hut', 'idcard' => 36, 'bankcard' => '260', 'error_code' => 400, 'reason' => 'error'],
         ];
         ksort($result);
+
+        /************************* 2. 写入 Excel 文件 ******************************/
+        // 首先创建一个新的对象  PHPExcel object
+        $objPHPExcel = new Spreadsheet();
+
+        /** 以下是一些设置 ，什么作者、标题信息 */
+        $objPHPExcel->getProperties()->setCreator('lisgroup')
+            ->setLastModifiedBy('lisgroup')
+            ->setTitle('EXCEL 导出')
+            ->setSubject('EXCEL 导出')
+            ->setDescription('导出数据')
+            ->setKeywords("excel php")
+            ->setCategory("result file");
+        /*以下就是对处理Excel里的数据， 横着取数据，主要是这一步，其他基本都不要改*/
+
+        // Excel的第A列，uid是你查出数组的键值，下面以此类推
+        $setActive = $objPHPExcel->setActiveSheetIndex(0);
+        // 第一行应该是 param 参数
+        foreach ($result as $key => $value) {
+            if ($value['error_code'] == 0) {
+                $message = $value['result']['res'] == 1 ? '一致' : '不一致';
+            } else {
+                $message = $value['reason'];
+            }
+
+            $num = $key + 2;
+            $setActive->setCellValue('A'.$num, "\t".$value['name'])
+                ->setCellValue('B'.$num, "\t".$value['idcard'])
+                ->setCellValue('C'.$num, "\t".$value['bankcard'])
+                ->setCellValue('D'.$num, $message);
+
+            //sleep(0.15);
+        }
+
+        //得到当前活动的表,注意下文教程中会经常用到$objActSheet
+        $objActSheet = $objPHPExcel->getActiveSheet();
+        // 位置bbb  *为下文代码位置提供锚
+        // 给当前活动的表设置名称
+        $objActSheet->setTitle('Simple');
+        // 代码还没有结束，可以复制下面的代码来决定我们将要做什么
+
+        // 1,直接生成一个文件
+        $objWriter = IOFactory::createWriter($objPHPExcel, 'Xlsx');
+        $objWriter->save('out-207-'.date('mdHis').'.xlsx');
+
 
         $data = $this->request->all();
         // $data = ['id' => 2, 'api_excel_id' => 1, 'appkey' => '123','upload_url' => '/storage/20190130_114747_5c511e632efe8.xlsx', 'state' => 0];
---
+++ laravel/excel.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World !');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello world.xlsx');
+
+echo 'ok';
