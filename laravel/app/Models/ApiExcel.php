19c6eee72e813e12919a213807f699655331a0c0
lisgroup <407505297@qq.com>
2019-02-12 17:15:41 +0800

    fix(model): 多对一关联模型

--- laravel/app/Models/ApiExcel.php
+++ laravel/app/Models/ApiExcel.php
@@ -14,4 +14,14 @@ class ApiExcel extends Model
     protected $table = 'api_excel';
 
     protected $fillable = ['appkey', 'concurrent', 'api_param_id', 'upload_url', 'finish_url', 'description', 'uid', 'state'];
+
+    /**
+     * 获取关联关系
+     *
+     * @return mixed
+     */
+    public function apiParam()
+    {
+        return $this->belongsTo(ApiParam::class, 'api_param_id');
+    }
 }
