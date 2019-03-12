ac27dbff529cbdcc7b8148fab0d13a5c99a219e3
lisgroup <407505297@qq.com>
2019-03-12 17:04:24 +0800

    fix(control): 修改密码需要验证权限

--- laravel/app/Http/Controllers/Api/UserController.php
+++ laravel/app/Http/Controllers/Api/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
         // 这样的结果是，token 只能在有效期以内进行刷新，过期无法刷新
         // 如果把 refresh 也放进去，token 即使过期但仍在刷新期以内也可刷新
         // 不过刷新一次作废
-        $this->middleware('auth:api', ['except' => ['password']]);
+        $this->middleware('auth:api', ['except' => ['login']]);
         // 另外关于上面的中间件，官方文档写的是『auth:api』
         // 但是我推荐用 『jwt.auth』，效果是一样的，但是有更加丰富的报错信息返回
     }
