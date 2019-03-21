bc9b5fefde9682910994b09711c308e6f5d3ec5e
lisgroup <407505297@qq.com>
2019-03-21 21:26:36 +0800

    fix(view): roles

--- admin/src/router/router.js
+++ admin/src/router/router.js
@@ -23,20 +23,20 @@ export const routeManage = [
     component: Layout,
     redirect: '/api_excel/index',
     name: 'Excel-List',
-    meta: { title: '批量测试管理', icon: 'ico-table', role: Admin },
+    meta: { title: '批量测试管理', icon: 'ico-table', roles: [Super, Admin] },
     children: [
       { path: '/api_excel/edit/:id', name: 'EditExcel', component: () => import('@/views/api_excel/edit'), hidden: true },
       {
         path: '/api_excel/add',
         name: 'AddExcel',
         component: () => import('@/views/api_excel/add'),
-        meta: { title: '上传测试', icon: 'excel', role: Admin }
+        meta: { title: '上传测试', icon: 'excel', roles: [Super, Admin] }
       },
       {
         path: '/api_excel/index',
         name: 'Excel',
         component: () => import('@/views/api_excel/index'),
-        meta: { title: '测试管理', icon: 'ico-aliyun', role: Admin }
+        meta: { title: '测试管理', icon: 'ico-aliyun', roles: [Super, Admin] }
       },
       { path: '/api_param/add', name: 'AddApiParam', component: () => import('@/views/api_param/add'), hidden: true },
       { path: '/api_param/edit/:id', name: 'EditApiParam', component: () => import('@/views/api_param/edit'), hidden: true },
@@ -44,7 +44,7 @@ export const routeManage = [
         path: '/api_param/index',
         name: 'ApiParam',
         component: () => import('@/views/api_param/index'),
-        meta: { title: '接口列表', icon: 'api', role: Admin }
+        meta: { title: '接口列表', icon: 'api', roles: [Super, Admin] }
       }
     ]
   }
@@ -58,7 +58,7 @@ export const routerAdmin = [
     component: Layout,
     redirect: '/category/index',
     name: 'Category-Nav',
-    meta: { title: '栏目菜单', icon: 'category', role: Super },
+    meta: { title: '栏目菜单', icon: 'category', roles: [Super] },
     children: [
       { path: '/category/add', name: 'AddCategory', component: () => import('@/views/category/add'), meta: { title: '添加栏目' }, hidden: true },
       { path: '/category/edit/:id', name: 'EditCategory', component: () => import('@/views/category/edit'), hidden: true },
@@ -66,7 +66,7 @@ export const routerAdmin = [
         path: '/category/index',
         name: 'Category',
         component: () => import('@/views/category/index'),
-        meta: { title: '栏目管理', icon: 'ico-category', role: Super }
+        meta: { title: '栏目管理', icon: 'ico-category', roles: [Super] }
       },
       { path: '/nav/add', name: 'AddNav', component: () => import('@/views/nav/add'), hidden: true },
       { path: '/nav/edit/:id', name: 'EditNav', component: () => import('@/views/nav/edit'), hidden: true },
@@ -74,7 +74,7 @@ export const routerAdmin = [
         path: '/nav',
         name: 'Nav',
         component: () => import('@/views/nav'),
-        meta: { title: '导航管理', icon: 'nav', role: Super }
+        meta: { title: '导航管理', icon: 'nav', roles: [Super] }
       },
       { path: '/tag/add', name: 'AddTag', component: () => import('@/views/tag/add'), meta: { title: '添加标签' }, hidden: true },
       { path: '/tag/edit/:id', name: 'EditTag', component: () => import('@/views/tag/edit'), hidden: true },
@@ -82,7 +82,7 @@ export const routerAdmin = [
         path: '/tag',
         name: 'Tag',
         component: () => import('@/views/tag/index'),
-        meta: { title: '标签列表', icon: 'tag', role: Super }
+        meta: { title: '标签列表', icon: 'tag', roles: [Super] }
       }
     ]
   },
@@ -92,20 +92,20 @@ export const routerAdmin = [
     component: Layout,
     // redirect: '/article',
     name: 'Article-List',
-    meta: { title: '文章管理', icon: 'article', role: Super },
+    meta: { title: '文章管理', icon: 'article', roles: [Super] },
     children: [
       { path: '/article/edit/:id', name: 'EditArticle', component: () => import('@/views/article/edit'), hidden: true },
       {
         path: '/article/index',
         name: 'Article',
         component: () => import('@/views/article/index'),
-        meta: { title: '文章管理', icon: 'ico-article', role: Super }
+        meta: { title: '文章管理', icon: 'ico-article', roles: [Super] }
       },
       {
         path: '/article/add',
         name: 'AddArticle',
         component: () => import('@/views/article/add'),
-        meta: { title: '添加文章', icon: 'add', role: Super }
+        meta: { title: '添加文章', icon: 'add', roles: [Super] }
       }
     ]
   },
@@ -115,7 +115,7 @@ export const routerAdmin = [
     component: Layout,
     redirect: '/task',
     name: '公交',
-    meta: { title: '公交管理', icon: 'bus', role: Super },
+    meta: { title: '公交管理', icon: 'bus', roles: [Super] },
     children: [
       { path: '/task/search', name: 'search', component: () => import('@/views/task/search'), hidden: true },
       { path: '/task/newBus', name: 'NewBus', component: () => import('@/views/task/newBus'), hidden: true },
@@ -124,7 +124,7 @@ export const routerAdmin = [
         path: '/task',
         name: '定时任务',
         component: () => import('@/views/task/index'),
-        meta: { title: '定时任务', icon: 'task', role: Super }
+        meta: { title: '定时任务', icon: 'task', roles: [Super] }
       },
       { path: 'lines/add', name: 'linesAdd', component: () => import('@/views/lines/add'), hidden: true },
       { path: 'lines/edit/:id', name: 'linesEdit', component: () => import('@/views/lines/edit'), hidden: true },
@@ -132,19 +132,19 @@ export const routerAdmin = [
         path: 'lines',
         name: '公交列表',
         component: () => import('@/views/lines/index'),
-        meta: { title: '公交列表', icon: 'table', role: Super }
+        meta: { title: '公交列表', icon: 'table', roles: [Super] }
       },
       {
         path: 'config',
         name: '配置管理',
         component: () => import('@/views/lines/index'),
-        meta: { title: '配置列表', icon: 'table', role: Super }
+        meta: { title: '配置列表', icon: 'table', roles: [Super] }
       },
       {
         path: 'tree',
         name: 'Tree',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree', role: Super },
+        meta: { title: 'Tree', icon: 'tree', roles: [Super] },
         hidden: true
       }
     ]
@@ -155,7 +155,7 @@ export const routerAdmin = [
     component: Layout,
     redirect: '/user',
     name: '权限',
-    meta: { title: '权限管理', icon: 'auth', role: Super },
+    meta: { title: '权限管理', icon: 'auth', roles: [Super] },
     children: [
       { path: 'index', name: 'userIndex', component: () => import('@/views/user/index'), hidden: true },
       { path: 'password', name: 'userPassword', component: () => import('@/views/user/password'), hidden: true },
@@ -165,7 +165,7 @@ export const routerAdmin = [
         path: '/user',
         name: '用户管理',
         component: () => import('@/views/user/index'),
-        meta: { title: '用户列表', icon: 'user', role: Super }
+        meta: { title: '用户列表', icon: 'user', roles: [Super] }
       },
       { path: '/permission/add', name: 'AddPermission', component: () => import('@/views/permission/add'), hidden: true },
       { path: '/permission/edit/:id', name: 'EditPermission', component: () => import('@/views/permission/edit'), hidden: true },
@@ -173,16 +173,16 @@ export const routerAdmin = [
         path: '/permission',
         name: '权限列表',
         component: () => import('@/views/permission/index'),
-        meta: { title: '权限列表', icon: 'permission', role: Super }
+        meta: { title: '权限列表', icon: 'permission', roles: [Super] }
       },
 
-      { path: '/role/add', name: 'AddRole', component: () => import('@/views/role/add'), hidden: true },
-      { path: '/role/edit/:id', name: 'EditRole', component: () => import('@/views/role/edit'), hidden: true },
+      { path: '/role/add', name: 'Addroles', component: () => import('@/views/role/add'), hidden: true },
+      { path: '/role/edit/:id', name: 'Editroles', component: () => import('@/views/role/edit'), hidden: true },
       {
         path: '/role',
         name: '角色管理',
         component: () => import('@/views/role/index'),
-        meta: { title: '角色管理', icon: 'role', role: Super }
+        meta: { title: '角色管理', icon: 'roles', roles: [Super] }
       }
     ]
   }
