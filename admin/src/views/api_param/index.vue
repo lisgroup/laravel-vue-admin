1b2df5009c15dd771096d7d335ce3743fe4d2cb3
lisgroup <407505297@qq.com>
2019-03-22 16:22:30 +0800

    fix(view): 显示删除按钮

--- admin/src/views/api_param/index.vue
+++ admin/src/views/api_param/index.vue
@@ -61,10 +61,10 @@
           <el-button
             size="mini"
             @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <!--<el-button-->
-            <!--size="mini"-->
-            <!--type="danger"-->
-            <!--@click="handleDelete(scope.$index, scope.row)">删除</el-button>-->
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
