4c7a43639339127b3e89ffcef86b0b94b743fb22
lisgroup <407505297@qq.com>
2019-03-25 16:41:37 +0800

    fix(view): 登录页面

--- admin/src/views/login/index.vue
+++ admin/src/views/login/index.vue
@@ -36,10 +36,6 @@
           Sign in
         </el-button>
       </el-form-item>
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: 123456</span>
-      </div>
     </el-form>
   </div>
 </template>
@@ -68,8 +64,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '123456'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
