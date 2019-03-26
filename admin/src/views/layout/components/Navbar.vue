d1c8d19b1d45b1f13a0ba825d703ebe387d1daaa
lisgroup <407505297@qq.com>
2019-03-26 11:39:13 +0800

    fix(view): 退出登录跳转登录页

--- admin/src/views/layout/components/Navbar.vue
+++ admin/src/views/layout/components/Navbar.vue
@@ -75,7 +75,8 @@ export default {
     },
     logout() {
       this.$store.dispatch('LogOut').then(() => {
-        location.reload() // 为了重新实例化vue-router对象 避免bug
+        this.$router.push({ path: '/login?redirect=/admin/dashboard' })
+        // location.reload() // 为了重新实例化vue-router对象 避免bug
       })
     }
   }
