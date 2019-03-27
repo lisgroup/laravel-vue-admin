1c51d3ff33b395ee0075e48adcc338277e03ed1b
lisgroup <407505297@qq.com>
2019-03-27 18:19:07 +0800

    update: uid

--- laravel/database/migrations/2019_01_28_205011_create_api_excel_table.php
+++ laravel/database/migrations/2019_01_28_205011_create_api_excel_table.php
@@ -22,10 +22,11 @@ class CreateApiExcelTable extends Migration {
 			$table->string('upload_url', 255)->default('')->comment('上传地址');
 			$table->string('finish_url', 255)->default('')->comment('处理完成 url');
 			$table->string('description')->default('')->comment('描述');
-			$table->string('uid')->default('')->comment('上传用户');
+			$table->unsignedInteger('uid')->default('0')->comment('上传用户ID号');
 			$table->unsignedTinyInteger('state')->default('0')->comment('处理状态，0 新加入，1 开始处理；2 处理完成');
 			$table->timestamps();
 			$table->index('api_param_id', 'index_api_param_id');
+			$table->index('uid', 'index_uid');
 		});
 	}
 
