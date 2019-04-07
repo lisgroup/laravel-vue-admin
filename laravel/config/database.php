8162dd55ad34f411eaae26d710d4419a60b0248c
lisgroup <407505297@qq.com>
2019-04-07 16:48:43 +0800

    fix(config): 持久化连接

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -120,6 +120,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
+            'persistent' => true, // 开启持久连接
         ],
 
         'cache' => [
