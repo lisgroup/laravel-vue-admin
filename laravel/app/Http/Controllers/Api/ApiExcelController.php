dfc8bd2b4df12caa7bde5395247ee8e4674bb083
lisgroup <407505297@qq.com>
2019-04-08 15:52:23 +0800

    fix(control): 控制器输出获取数据

--- laravel/app/Http/Controllers/Api/ApiExcelController.php
+++ laravel/app/Http/Controllers/Api/ApiExcelController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Events\ApiExcelEvent;
-use App\Http\Repository\MultithreadingRepository;
+use App\Http\Repository\ExcelRepository;
 use App\Http\Requests\ApiExcel\Store;
 use App\Http\Requests\ApiExcel\Update;
 use App\Models\ApiExcel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class ApiExcelController extends Controller
 {
@@ -34,7 +32,7 @@ class ApiExcelController extends Controller
         // 这样的结果是，token 只能在有效期以内进行刷新，过期无法刷新
         // 如果把 refresh 也放进去，token 即使过期但仍在刷新期以内也可刷新
         // 不过刷新一次作废
-        $this->middleware(['auth:api', 'role'], ['except' => ['upload']]);
+        $this->middleware(['auth:api', 'role'], ['except' => ['login']]);
         // 另外关于上面的中间件，官方文档写的是『auth:api』
         // 但是我推荐用 『jwt.auth』，效果是一样的，但是有更加丰富的报错信息返回
 
@@ -264,5 +262,22 @@ class ApiExcelController extends Controller
         ApiExcel::onlyTrashed()->find($id)->forceDelete();
 
         return $this->out(200);
+    }
+
+    public function downloadLog()
+    {
+        $api_excel_id = $this->request->input('id');
+        // 判断用户有没有下载权限
+        $user_id = auth('api')->user()['id'];
+        // $user_id = 1;
+        $failed_done_file = ExcelRepository::getInstent()->exportExcelLogs($api_excel_id, $user_id);
+        if ($failed_done_file === false) {
+            // 权限不足
+            return $this->out(4001);
+        } elseif ($failed_done_file === '') {
+            // 无可下载内容
+            return $this->out(4009, [], '下载失败，无可下载内容');
+        }
+        return $this->out(200, ['failed_done_file' => $failed_done_file]);
     }
 }
