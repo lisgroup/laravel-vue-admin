039fe17dfe21e85eef0a75c38819aebca333c3ba
lisgroup <407505297@qq.com>
2019-04-19 16:15:23 +0800

    fix: 系统事件重置或销毁
    可以在这些事件中重置或销毁一些全局或静态的变量，也可以修改当前的请求和响应

--- laravel/app/Providers/EventServiceProvider.php
+++ laravel/app/Providers/EventServiceProvider.php
@@ -33,6 +33,15 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        // 修改`app/Providers/EventServiceProvider.php`, 添加下面监听代码到boot方法中
+        // 如果变量$events不存在，你也可以通过Facade调用\Event::listen()。
+        // \Event::listen('laravels.received_request', function (\Illuminate\Http\Request $req, $app) {
+        //     $req->query->set('get_key', 'hhxsv5');// 修改querystring
+        //     $req->request->set('post_key', 'hhxsv5'); // 修改post body
+        // });
+
+        // \Event::listen('laravels.generated_response', function (\Illuminate\Http\Request $req, \Symfony\Component\HttpFoundation\Response $rsp, $app) {
+        //     $rsp->headers->set('header-key', 'hhxsv5');// 修改header
+        // });
     }
 }
