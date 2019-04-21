83142dc0c1a37ac23efde193f47c8d59e2bf66bb
lisgroup <407505297@qq.com>
2019-04-21 15:33:18 +0800

    chore(view): 构建前端

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -45,6 +45,7 @@ Route::group(['namespace' => 'Bus', 'prefix' => 'api'], function () {
     Route::any('line', 'NewApiController@getList');
     Route::any('new_line', 'NewApiController@newBusLine');
     Route::any('output', 'NewApiController@output');
+    Route::any('test', 'NewApiController@Task');
 });
 
 /******** 测试任务的接口地址 *********/
