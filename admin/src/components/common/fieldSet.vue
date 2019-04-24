8247129d5b9a02f8088b53bdadc66aa795c0bf92
lisgroup <407505297@qq.com>
2019-04-24 16:43:10 +0800

    add: 子组件

---
+++ admin/src/components/common/fieldSet.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <blockquote class="layui-elem-quote">
+      苏州公交实时查询信息（仅供参考）
+    </blockquote>
+    <fieldset class="layui-elem-field layui-field-title" style="margin-top: 20px;">
+      <legend>苏州公交实时查询信息（仅供参考）</legend>
+    </fieldset>
+  </div>
+</template>
