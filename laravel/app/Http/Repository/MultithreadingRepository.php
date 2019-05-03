09007fe1439ff5d95a33dd49f2374b8363ff20d1
lisgroup <407505297@qq.com>
2019-05-03 14:06:41 +0800

    fix(repo): 暂不使用队列

--- laravel/app/Http/Repository/MultithreadingRepository.php
+++ laravel/app/Http/Repository/MultithreadingRepository.php
@@ -394,8 +394,8 @@ class MultithreadingRepository
                     'result' => $result,
                     'created_at' => date('Y-m-d H:i:s'),
                 ];
-                // ApiExcelLogs::insert($logs);
-                event(new SaveApiExcelLogEvent($logs));
+                ApiExcelLogs::insert($logs);
+                // event(new SaveApiExcelLogEvent($logs));
                 // var_dump($result);
                 // var_dump($index);
                 $this->data[$index] = $result;
@@ -409,8 +409,8 @@ class MultithreadingRepository
                     'result' => '',
                     'created_at' => date('Y-m-d H:i:s'),
                 ];
-                // ApiExcelLogs::insert($logs);
-                event(new SaveApiExcelLogEvent($logs));
+                ApiExcelLogs::insert($logs);
+                // event(new SaveApiExcelLogEvent($logs));
                 // this is delivered each failed request
                 if (is_object($reason) && is_callable([$reason, 'getMessage'])) {
                     $reason = 'Line:'.$reason->getLine().' in '.$reason->getFile().'; Message: '.$reason->getMessage();
