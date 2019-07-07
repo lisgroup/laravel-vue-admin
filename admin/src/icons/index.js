a0d879534631ccf62f6c06331c1d846e23115056
lisgroup <407505297@qq.com>
2019-07-07 16:03:04 +0800

    add: svg

--- admin/src/icons/index.js
+++ admin/src/icons/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
+import NavBar from '@/components/common/navBar'
 
 // register globally
 Vue.component('svg-icon', SvgIcon)
+Vue.component('nav-bar', NavBar)
 
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
