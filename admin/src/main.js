2823313dab87a51296898cdba263e4ad6c55c9fd
lisgroup <407505297@qq.com>
2019-07-08 10:32:17 +0800

    fix(view): default setting

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 // 引入 layUI css
@@ -18,7 +18,8 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
