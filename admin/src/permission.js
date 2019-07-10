b20803aac1ac51bd9b63983428915f3c78d14d7c
lisgroup <407505297@qq.com>
2019-07-10 17:56:27 +0800

    fix(view): remove log

--- admin/src/permission.js
+++ admin/src/permission.js
@@ -41,7 +41,6 @@ router.beforeEach(async(to, from, next) => {
 
           // dynamically add accessible routes
           router.addRoutes(accessRoutes)
-          console.log(router)
 
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
