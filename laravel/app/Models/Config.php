38bfb291d7aa07b4b6a9e8a3ef8d4df53bb41511
lisgroup <407505297@qq.com>
2020-09-10 13:41:25 +0800

    chore: add model

---
+++ laravel/app/Models/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Config extends Model
+{
+    /**
+     * 与模型关联的数据表
+     *
+     * @var string
+     */
+    protected $table = 'config';
+}
